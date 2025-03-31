157f1d174e282b877d7288713b3be7f105c5facc
Thomas Nathan Feeney <tnfeeney@hotmail.com>
2025-03-31 21:47:32 +0100

    mortgage calculator

---
+++ portfolio-vue-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  
+  <ProjectCarousel />
+</template>
+
+<script>
+import ProjectCarousel from './components/VueperCarousel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProjectCarousel
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ portfolio-vue-app/src/components/VueperCarousel.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <vueper-slides class="no-shadow" arrows-outside bullets-outside transition-speed="250">
+      <vueper-slide
+        v-for="i in 6"
+        :key="i"
+        :title="i.toString()"
+        :style="'background-color: ' + ['#ff5252', '#42b983'][i % 2]" />
+    </vueper-slides>
+  </div>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
+
+
+export default {
+  name: 'ProjectCarousel',
+  components: { VueperSlides, VueperSlide }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container{
+  margin: 0 10%;
+}
+.ex--center-mode {
+  width: 600px;
+  max-width: 100%;
+  margin: auto;
+}
+</style>
