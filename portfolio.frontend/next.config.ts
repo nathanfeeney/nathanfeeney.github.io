9ee60bfdd3774d743dc45e56a03bac21eead8466
Thomas Nathan Feeney <tnfeeney@hotmail.com>
2025-12-08 21:18:24 +0000

    updated path

--- portfolio.frontend/next.config.ts
+++ portfolio.frontend/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   output: 'export'
+   output: 'export',
+   basePath: '/nathanfeeney.github.io',  
+  assetPrefix: '/nathanfeeney.github.io', 
+
 };
 
 export default nextConfig;
