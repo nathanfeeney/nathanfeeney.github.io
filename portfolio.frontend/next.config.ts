4e727397859e118677f2026fe0d2442b6ccf96e1
Thomas Nathan Feeney <tnfeeney@hotmail.com>
2025-12-08 21:23:43 +0000

    retrying with new config

--- portfolio.frontend/next.config.ts
+++ portfolio.frontend/next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    output: 'export',
-   basePath: '/nathanfeeney.github.io',  
-  assetPrefix: '/nathanfeeney.github.io', 
-
 };
 
 export default nextConfig;
